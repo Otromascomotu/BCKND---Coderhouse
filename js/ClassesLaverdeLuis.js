2b76b257a75d36773770c1208b0b636f97df9238
luislaverde <luislaverde@gmail.com>
2021-08-03 20:31:42 -0300

    Correción de Desafío 1: Funciones Constructoras y Clases

--- js/ClassesLaverdeLuis.js
+++ js/ClassesLaverdeLuis.js
@@ -1,42 +1,48 @@
+//Se crea un objeto llamado usuario a partir de la función constructora, con valores que permitan probar todos sus métodos
 let usuario = new Usuario('darth','vader', ['dumbo', 'mufasa', 'bambi', 'simba'], [{'it': 'stephenKing'}, {'elPrincipito': 'antoineDeSaintExupery'}, {'seda': 'alessandroBaricco'}, {'laMetamorfosis': 'franzKafka'}]);
-let nombre = 'luis';
-let apellido = 'laverde';
-const libro = {book: 'ViajeAlCentroDeLaTierra', autor: 'JulioVerne'}
-let libros = [{'nostromo': 'josephConrad'}, {'padreRicoPadrePobre': 'robertKiyosaki'}, {'laOdisea': 'homero'}, {'otelo: williamShakespeare'}, {'divinaComedia': 'danteAlihieri'}, {'elCuervo': 'edgarAlanPoe'}];
-let mascotas = ['tico', 'sparkie', 'bobby', 'charlie', 'zeus', 'twinkie'];
 
+//Se declara una función de clase llamada Usuario que recibe como parámetros: nombre (string), apellido (string), libros (array de objetos) y mascotas (array de strings).
 class Usuario {
   constructor(nombre, apellido, mascotas, libros){
+    //Luego se implementan las propiedades instancia desde los valores recibidos como parámetro(argumento)
     this.nombre = nombre;
     this.apellido = apellido;
     this.mascotas = mascotas;
     this.libros = libros;
   }
-
+  
   getFullName(){
+    //Se retorna un string con el nombre y apellido del usuario. (utilizando el formato de template string)
     return `${this.nombre} ${this.apellido}`
   }
-
+  
   addMascota(mascota){
-    mascotas.push(mascota)
+    let mascotas = ['tico', 'sparkie', 'bobby', 'charlie', 'zeus', 'twinkie'];
+    //Se agrega una mascota (mascota) al arreglo de mascotas del usuario. No se retorna nada.
+    this.mascotas.push(mascota)
   }
-
+  
   getMascotas(){
+    // Se retorna la cantidad de mascotas que tiene el usuario.
     return mascotas.lenght
   }
-
+  
   addBook(book, autor){
-    this.libros.push({book, autor})
+    //Se recibe un string 'book' y un string 'autor' y debe agregar un objeto: { nombre: book, autor: autor} al arreglo de libros del usuario. No debe retornar nada.
+    const libro = {book: 'ViajeAlCentroDeLaTierra', autor: 'JulioVerne'}
+    this.libros.push(book, autor)
   }
-
+  
   getBooks(){
+    const libros = [{'nostromo': 'josephConrad'}, {'padreRicoPadrePobre': 'robertKiyosaki'}, {'laOdisea': 'homero'}, {'otelo': 'williamShakespeare'}, {'divinaComedia': 'danteAlihieri'}, {'elCuervo': 'edgarAlanPoe'}];
+    //Se retorna un arreglo con sólo los nombres del arreglo de libros del usuario.
     return this.libros.map((value) => value.book)
   }
 }
 
-
 console.log(usuario.getFullName());
-console.log(usuario.addMascota('Negro'));
 console.log(usuario.getMascotas());
-console.log(usuario.addBook('prometeoEncadenado','esquilo'));
 console.log(usuario.getBooks());
+
+//Se hace el test completo del objeto instancaido en la línea 2, empleando la consola del navegador.
+console.log(usuario)
--- js/funcionConstructoraLaverdeLuis.js
+++ js/funcionConstructoraLaverdeLuis.js
@@ -1,42 +1,49 @@
-const usuario = new Usuario('luke','skywalker',['acero', 'dorothy', 'beto', 'monti'], [{titulo: 'elAlquimista', autor: 'pauloCohelo'}, {titulo: 'elCapital', autor: 'karlMarx'}, {titulo: 'elCodigoDaVinci', autor: 'danBrown'}, {titulo:'caballoDeTroya', autor: 'juanJoseBenitezLopez'}]);
+//Se crea un objeto llamado usuario2 a partir de la función constructora, con valores que permitan probar todos sus métodos
+let usuario2 = new Usuario2('luke','skywalker',['acero', 'dorothy', 'beto', 'monti'], [{titulo: 'elAlquimista', autor: 'pauloCohelo'}, {titulo: 'elCapital', autor: 'karlMarx'}, {titulo: 'elCodigoDaVinci', autor: 'danBrown'}, {titulo:'caballoDeTroya', autor: 'juanJoseBenitezLopez'}]);
 
-function Usuario(nombre, apellido, mascotas, libros){
+
+//Se declara una función constructora llamada Usuario que recibe como parámetros: nombre (string), apellido (string), libros (array de objetos) y mascotas (array de strings). 
+function Usuario2(nombre, apellido, mascotas, libros){
+  //Luego se implementan las propiedades instancia desde los valores recibidos como parámetro(argumento)
   this.nombre = nombre;
   this.apellido = apellido;
   this.mascotas = mascotas;
   this.libros = libros;
-  
 }
 
-Usuario.prototype.getFullName = function(){
+Usuario2.prototype.getFullName = function(){
+  //Se retorna un string con el nombre y apellido del usuario. (utilizando el formato de template string).
   return `${this.nombre} ${this.apellido}`
 }
 
-Usuario.prototype.addMascota = function(mascota){
-  //const mascotas = ['tico', 'sparkie', 'bobby', 'charlie', 'zeus', 'twinkie']
+Usuario2.prototype.addMascota = function(mascota){
+  const mascotas = ['tico', 'sparkie', 'bobby', 'charlie', 'zeus', 'twinkie']
+  //Se agrega una mascota (mascota) al arreglo de mascotas del usuario. No se retorna nada.
   this.mascotas.push(mascota);
-  this.addMascota('rockie')
 }
 
-Usuario.prototype.getMascotas = function(){
+Usuario2.prototype.getMascotas = function(){
+  // Se retorna la cantidad de mascotas que tiene el usuario.
   return this.mascotas.length
 }
 
-Usuario.prototype.addBook = function(book, autor){
-  //const libros = [{'nostromo': 'josephConrad'}, {'padreRicoPadrePobre': 'robertKiyosaki'}, {'laOdisea': 'homero'}, {'otelo': 'williamShakespeare'}, {'divinaComedia': 'danteAlihieri'}, {'elCuervo': 'edgarAlanPoe'}];
+Usuario2.prototype.addBook = function(book, autor){
+  //Se recibe un string 'book' y un string 'autor' y debe agregar un objeto: { nombre: book, autor: autor} al arreglo de libros del usuario. No debe retornar nada.
+  const libro = [{book: 'ViajeAlCentroDeLaTierra', autor: 'JulioVerne'}]
   this.libros.push(book, autor)
 }
 
-Usuario.prototype.getBooks = function(){
-  //const libro = [{book: 'ViajeAlCentroDeLaTierra', autor: 'JulioVerne'}]
-  return this.libros.map((value) => value.book)
+Usuario2.prototype.getBooks = function(){
+  const libros = [{'nostromo': 'josephConrad'}, {'padreRicoPadrePobre': 'robertKiyosaki'}, {'laOdisea': 'homero'}, {'otelo': 'williamShakespeare'}, {'divinaComedia': 'danteAlihieri'}, {'elCuervo': 'edgarAlanPoe'}];
+  //Se retorna un arreglo con sólo los nombres del arreglo de libros del usuario.  
+  return this.libros.map((value) => value.titulo)
 }
 
+console.log(usuario2.getFullName());
+console.log(usuario2.getMascotas());
+console.log(usuario2.getBooks());
 
-console.log(usuario.getFullName());
-//console.log(usuario.addMascota('rockie'));
-console.log(usuario.getMascotas());
-console.log(usuario.addBook('Sherezade','Anonimo'));
-console.log(usuario.getBooks());
+//Se hace el test completo del objeto instancaido en la línea 2, empleando la consola del navegador.
+console.log(usuario2)
 
 
